71d8ed5f3bc0fea5cbf66cf044ed01f91a606a00
Sebastian Pipping <sebastian@pipping.org>
2024-05-11 12:11:55 +0200

    doc/preprocess.sh: Add "set -x" to ease future debugging

--- doc/preprocess.sh
+++ doc/preprocess.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
 # Run GCC preprocessor and delete empty lines
 : ${CC:=cc}
+PS4='doc/preprocess.sh|# '
+set -x
 "${CC}" -E -DURI_DOXYGEN -DURI_NO_UNICODE -C -I ../include "$1" | sed -e '/^$/d' -e 's/COMMENT_HACK//g'
