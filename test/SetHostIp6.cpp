bbc7883fbcad5bac05969be09994f2bb9d2fce1b
Sebastian Pipping <sebastian@pipping.org>
2025-08-25 23:24:24 +0200

    tests: Cover SetHostIp6

--- test/SetHostIp6.cpp
+++ test/SetHostIp6.cpp
@@ -35,6 +35,29 @@ static void testIsWellFormedHostIp6(const char * candidate, bool expectedWellFor
 	ASSERT_EQ(actualWellFormed, expectedWellFormed);
 }
 
+static UriUriA parseWellFormedUri(const char * text) {
+	UriUriA uri;
+	const int error = uriParseSingleUriA(&uri, text, NULL);
+	// NOTE: we cannot use ASSERT_EQ here because of the outer non-void return type
+	assert(error == URI_SUCCESS);
+	return uri;
+}
+
+static void assertUriEqual(const UriUriA * uri, const char * expected) {
+	int charsRequired = -1;
+	ASSERT_EQ(uriToStringCharsRequiredA(uri, &charsRequired), URI_SUCCESS);
+	ASSERT_TRUE(charsRequired >= 0);
+
+	char * const buffer = (char *)malloc(charsRequired + 1);
+	ASSERT_TRUE(buffer != NULL);
+
+	ASSERT_EQ(uriToStringA(buffer, uri, charsRequired + 1, NULL), URI_SUCCESS);
+
+	EXPECT_STREQ(buffer, expected);
+
+	free(buffer);
+}
+
 }  // namespace
 
 TEST(IsWellFormedHostIp6, Null) {
@@ -143,4 +166,208 @@ TEST(IsWellFormedHostIp6, NonHex) {
 TEST(IsWellFormedHostIp6, IpFuture) {
     testIsWellFormedHostIp6("v7.host", false);
     testIsWellFormedHostIp6("V7.host", false);
+}
+
+TEST(SetHostIp6, NullUriOnly) {
+	UriUriA * const uri = NULL;
+	const char * const first = "::1";
+	const char * const afterLast = first + strlen(first);
+	ASSERT_EQ(uriSetHostIp6A(uri, first, afterLast), URI_ERROR_NULL);
+}
+
+TEST(SetHostIp6, NullFirstOnly) {
+	UriUriA uri = {};
+	const char * const host = "::1";
+	const char * const first = NULL;
+	const char * const afterLast = host + strlen(host);
+	ASSERT_EQ(uriSetHostIp6A(&uri, first, afterLast), URI_ERROR_NULL);
+}
+
+TEST(SetHostIp6, NullAfterLastOnly) {
+	UriUriA uri = {};
+	const char * const first = "::1";
+	const char * const afterLast = NULL;
+	ASSERT_EQ(uriSetHostIp6A(&uri, first, afterLast), URI_ERROR_NULL);
+}
+
+TEST(SetHostIp6, NullValueLeavesOwnerAtFalse) {
+	UriUriA uri = parseWellFormedUri("scheme://host/");
+	EXPECT_EQ(uri.owner, URI_FALSE);  // self-test
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_SUCCESS);
+
+	EXPECT_EQ(uri.owner, URI_FALSE);  // i.e. still false
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NonNullValueMakesOwner) {
+	UriUriA uri = parseWellFormedUri("scheme://old/");
+	const char * const first = "::1";
+	const char * const afterLast = first + strlen(first);
+	EXPECT_EQ(uri.owner, URI_FALSE);  // self-test
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, first, afterLast), URI_SUCCESS);
+
+	EXPECT_EQ(uri.owner, URI_TRUE);  // i.e. now owned
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NullValueAppliedDotInserted) {
+	UriUriA uri = parseWellFormedUri("scheme://host//path1/path2");
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_SUCCESS);
+
+	assertUriEqual(&uri, "scheme:/.//path1/path2");  // i.e. not scheme://path1/path2
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NullValueAppliedDotNotInserted) {
+	UriUriA uri = parseWellFormedUri("//host/./path1/path2");
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_SUCCESS);
+
+	assertUriEqual(&uri, "/./path1/path2");  // i.e. not /././path1/path2
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NullValueAppliedPriorNull) {
+	UriUriA uri = parseWellFormedUri("scheme:/path");
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_SUCCESS);
+
+	assertUriEqual(&uri, "scheme:/path");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NullValueAppliedPriorIp4) {
+	UriUriA uri = parseWellFormedUri("scheme://1.2.3.4/path");
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_SUCCESS);
+
+	assertUriEqual(&uri, "scheme:/path");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NullValueAppliedPriorIp6) {
+	UriUriA uri = parseWellFormedUri("scheme://[::1]/path");
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_SUCCESS);
+
+	assertUriEqual(&uri, "scheme:/path");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NullValueAppliedPriorIpFuture) {
+	UriUriA uri = parseWellFormedUri("scheme://[v7.host]/path");
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_SUCCESS);
+
+	assertUriEqual(&uri, "scheme:/path");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NullValueAppliedPriorRegName) {
+	UriUriA uri = parseWellFormedUri("scheme://host/path");
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_SUCCESS);
+
+	assertUriEqual(&uri, "scheme:/path");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NonNullValueAppliedNonEmptyPriorNull) {
+	UriUriA uri = parseWellFormedUri("scheme:");
+	const char * const first = "::1";
+	const char * const afterLast = first + strlen(first);
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, first, afterLast), URI_SUCCESS);
+
+	assertUriEqual(&uri, "scheme://[0000:0000:0000:0000:0000:0000:0000:0001]");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NonNullValueAppliedNonEmptyPriorIp4) {
+	UriUriA uri = parseWellFormedUri("//1.2.3.4");
+	const char * const first = "::1";
+	const char * const afterLast = first + strlen(first);
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, first, afterLast), URI_SUCCESS);
+
+	assertUriEqual(&uri, "//[0000:0000:0000:0000:0000:0000:0000:0001]");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NonNullValueAppliedNonEmptyPriorIp6) {
+	UriUriA uri = parseWellFormedUri("//[::1]");
+	const char * const first = "::2";
+	const char * const afterLast = first + strlen(first);
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, first, afterLast), URI_SUCCESS);
+
+	assertUriEqual(&uri, "//[0000:0000:0000:0000:0000:0000:0000:0002]");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NonNullValueAppliedNonEmptyPriorIpFuture) {
+	UriUriA uri = parseWellFormedUri("//[v7.host]");
+	const char * const first = "::1";
+	const char * const afterLast = first + strlen(first);
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, first, afterLast), URI_SUCCESS);
+
+	assertUriEqual(&uri, "//[0000:0000:0000:0000:0000:0000:0000:0001]");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, NonNullValueAppliedNonEmptyPriorRegName) {
+	UriUriA uri = parseWellFormedUri("//hostname.test");
+	const char * const first = "::1";
+	const char * const afterLast = first + strlen(first);
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, first, afterLast), URI_SUCCESS);
+
+	assertUriEqual(&uri, "//[0000:0000:0000:0000:0000:0000:0000:0001]");
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, MalformedValueRejected) {
+	UriUriA uri = parseWellFormedUri("scheme://host/");
+	const char * const first = "not well-formed";
+	const char * const afterLast = first + strlen(first);
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, first, afterLast), URI_ERROR_SYNTAX);
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, UriWithPortRejected) {
+	UriUriA uri = parseWellFormedUri("//host:1234");
+	EXPECT_TRUE(uri.portText.first != NULL);  // self-test
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_ERROR_SETHOST_PORT_SET);
+
+	uriFreeUriMembersA(&uri);
+}
+
+TEST(SetHostIp6, UriWithUserInfoRejected) {
+	UriUriA uri = parseWellFormedUri("//user:password@host");
+	EXPECT_TRUE(uri.userInfo.first != NULL);  // self-test
+
+	EXPECT_EQ(uriSetHostIp6A(&uri, NULL, NULL), URI_ERROR_SETHOST_USERINFO_SET);
+
+	uriFreeUriMembersA(&uri);
 }
